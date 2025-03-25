74226e481288c2b0ddf99dfd3d5328fea986c11b
Yash Revannavar <106887362+YashRevannavar@users.noreply.github.com>
2025-03-25 21:53:57 +0100

    feat(project): Enhance logging for Git log retrieval and LLM summary generation

--- git_components/git_service.py
+++ git_components/git_service.py
@@ -1,19 +1,20 @@
 import os
 import subprocess
+import logging
 from environment.constants import root_path
-
 
 def get_git_repo_paths(root_path: str) -> list:
     """
     Returns a list of paths under root_path that are Git repositories.
     """
+    logging.info("Searching for Git repositories")
     git_repos = []
     for dirpath, dir_names, filenames in os.walk(root_path):
         if ".git" in dir_names:
             git_repos.append(dirpath)
             dir_names.remove(".git")
+    logging.info(f"Found {len(git_repos)} Git repositories")
     return git_repos
-
 
 def get_git_logs(days: int = 1) -> list:
     """
@@ -21,13 +22,13 @@ def get_git_logs(days: int = 1) -> list:
     :param days:
     :return:
     """
+    logging.info(f"Getting git logs for past {days} day(s)")
     list_of_logs_for_all_repos = []
     list_of_repos = get_git_repo_paths(root_path=root_path)
     for repo_path in list_of_repos:
         git_logs_for_single_repo = get_git_logs_for_single_repo(repo_path=repo_path, days=days)
         list_of_logs_for_all_repos.append(git_logs_for_single_repo)
     return list_of_logs_for_all_repos
-
 
 def get_git_logs_for_single_repo(repo_path: str, days: int = 1) -> str:
     """
@@ -36,12 +37,12 @@ def get_git_logs_for_single_repo(repo_path: str, days: int = 1) -> str:
     :param days:
     :return:
     """
+    logging.info(f"Getting logs for repository.")
     script_path = "git_components/git_connector.sh"
     command = [script_path, "--path", repo_path, "--days", str(days)]
     try:
         result = subprocess.run(command, check=True, capture_output=True, text=True)
         return result.stdout.strip()
     except subprocess.CalledProcessError as e:
-        print("❌ Error occurred:")
-        print(e.stderr)
+        logging.error(f"Error getting git logs: {e.stderr}")
         raise e
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import logging
 from git_components.git_service import get_git_logs
 from llm_components.llm_connector import llm_summary_generator
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
 if __name__ == "__main__":
+    logging.info("Starting EOD Generator")
     logs = get_git_logs(days=1)
+    logging.info("Git logs retrieved successfully")
     responses = llm_summary_generator(collected_commits=logs)
-    print(f"Responses: \n {responses}")
+    logging.info("Summary generated successfully")
+    logging.info(f"\n\n{responses}")
