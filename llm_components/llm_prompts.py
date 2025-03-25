101382afb149ccbf866222481d695fe0c520bc4a
Yash Revannavar <106887362+YashRevannavar@users.noreply.github.com>
2025-03-25 21:18:32 +0100

    fix(llm_prompts): Update prompt format for commit summarization

--- llm_components/llm_prompts.py
+++ llm_components/llm_prompts.py
@@ -10,4 +10,22 @@ to categorize it and provide a summary of the commits.
 
 Sometime it could be days worth of commits or just one day.
 You have to categorise the summary by each day.
+
+Format should be simple:
+
+- Date
+
+- Git Repo Name 1:
+- Branch Name 1...:
+- Summarised Task 1 
+- (type of commit) Summarised Task 2 
+- (type of commit) Summarised Task 3  so on...
+
+- Git Repo Name 2:
+- Branch Name 1...:
+- (type of commit) Summarised Task 1 
+- (type of commit) Summarised Task 2 
+- (type of commit) Summarised Task 3  so on...
+
+NOTE: Just follow the above format  no prefix or suffix is required.
 """
