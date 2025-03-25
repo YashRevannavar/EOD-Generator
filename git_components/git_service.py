3fa845db014aea464de36d9edd057634e3da4ed6
Yash Revannavar <106887362+YashRevannavar@users.noreply.github.com>
2025-03-25 21:18:52 +0100

    refactor(git-service): Simplify repo path retrieval and enhance logging functionality

--- git_components/git_service.py
+++ git_components/git_service.py
@@ -1,21 +1,47 @@
-def get_git_logs(repo_paths: list, days: int = 1) -> list:
+import os
+import subprocess
+from environment.constants import root_path
+
+
+def get_git_repo_paths(root_path: str) -> list:
+    """
+    Returns a list of paths under root_path that are Git repositories.
+    """
+    git_repos = []
+    for dirpath, dir_names, filenames in os.walk(root_path):
+        if ".git" in dir_names:
+            git_repos.append(dirpath)
+            dir_names.remove(".git")
+    return git_repos
+
+
+def get_git_logs(days: int = 1) -> list:
+    """
+    Returns a list of logs for all Git repositories under root_path.
+    :param days:
+    :return:
+    """
     list_of_logs_for_all_repos = []
-    for repo_path in repo_paths:
+    list_of_repos = get_git_repo_paths(root_path=root_path)
+    for repo_path in list_of_repos:
         git_logs_for_single_repo = get_git_logs_for_single_repo(repo_path=repo_path, days=days)
         list_of_logs_for_all_repos.append(git_logs_for_single_repo)
     return list_of_logs_for_all_repos
 
 
 def get_git_logs_for_single_repo(repo_path: str, days: int = 1) -> str:
-    import subprocess
-
+    """
+    Returns a list of logs for a single Git repository.
+    :param repo_path:
+    :param days:
+    :return:
+    """
     script_path = "git_components/git_connector.sh"
     command = [script_path, "--path", repo_path, "--days", str(days)]
-
     try:
         result = subprocess.run(command, check=True, capture_output=True, text=True)
         return result.stdout.strip()
     except subprocess.CalledProcessError as e:
         print("❌ Error occurred:")
         print(e.stderr)
-        return ""
+        raise e
