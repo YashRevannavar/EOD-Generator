02bce32b5014b85eb3a5da2fc15132eaeed34393
Yash Revannavar <106887362+YashRevannavar@users.noreply.github.com>
2025-03-25 21:52:26 +0100

    fear(project): Enhance logging for Git log retrieval and LLM summary generation

--- llm_components/llm_connector.py
+++ llm_components/llm_connector.py
@@ -1,25 +1,29 @@
 import logging
-
 from langchain_aws import ChatBedrock
 from langchain_core.messages import HumanMessage, SystemMessage
-
 from llm_components.llm_prompts import human_prompt, system_prompt
 
-
 def get_llm_bedrock_ai() -> ChatBedrock:
+    logging.info("Initializing ChatBedrock")
     return ChatBedrock(
         model_id="anthropic.claude-3-5-sonnet-20240620-v1:0",
         model_kwargs=dict(temperature=0.9),
     )
 
-
 def llm_summary_generator(collected_commits):
-    logging.info("Summarising news articles")
+    logging.info("Starting summary generation")
     chat = get_llm_bedrock_ai()
-    logging.info("Collecting prompt data")
-    messages = [HumanMessage(content=human_prompt, collected_commits=collected_commits), SystemMessage(content=system_prompt)]
+    
+    logging.info("Preparing prompts with commit data")
+    messages = [
+        HumanMessage(content=human_prompt, collected_commits=collected_commits),
+        SystemMessage(content=system_prompt)
+    ]
+    
     try:
+        logging.info("Sending request to LLM")
         response = chat.invoke(str(messages))
         return response.content
     except Exception as e:
-        return f"Due to {str(e)} we could not fetch the news data"
+        logging.error(f"Error generating summary: {str(e)}")
+        return f"Due to {str(e)} we could not generate the summary"
